167f7de0ab3152d24d39e10b29dc4cb34d99a717
Egor <olgin-24@mail.ru>
2016-12-19 02:56:14 +0300

    log

---
+++ src/main/java/ru/exercise01/util/ExtractLinks.java
@@ -0,0 +1,82 @@
+package ru.exercise01.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Egor on 19.12.16.
+ */
+public class ExtractLinks {
+
+
+    private List<String> linkList;
+    final private Pattern tagPattern;
+    final private Pattern linkPattern;
+    private Matcher tagMatcher;
+    private Matcher linkMatcher;
+    final private InputStreamReader inputStreamReader;
+
+    final private static String tagText = "(?i)<a([^>]+)>(.+?)>";
+    final private static String linkText = "\\s*(?i)href\\s*=\\s*(\\\"([^\"]*\\\")|'[^']*'|([^'\">\\s]+))";
+
+    public List<String> getLinkList() {
+        if (linkList == null) {
+            linkList = new ArrayList<>();
+        }
+        return linkList;
+    }
+
+    public Matcher getTagMatcher(String inStr) {
+        tagMatcher = tagPattern.matcher(inStr);
+        return tagMatcher;
+    }
+
+    public Matcher getLinkMatcher(String inStr) {
+        linkMatcher = linkPattern.matcher(inStr);
+        return linkMatcher;
+    }
+
+
+    public ExtractLinks(InputStreamReader readIn) {
+        inputStreamReader = readIn;
+        tagPattern = Pattern.compile(tagText);
+        linkPattern = Pattern.compile(linkText);
+        startWork();
+    }
+
+    private void startWork() {
+        try {
+            BufferedReader BufData = new BufferedReader(inputStreamReader);
+            String UrlData = null;
+            while ((UrlData = BufData.readLine()) != null) {
+                getLinkFromText(getTagMatcher(UrlData));
+            }
+        } catch (IOException io) {
+            System.out.println(io);
+
+        }
+
+
+    }
+
+    private void getLinkFromText(Matcher inMatcher) {
+        String outStr = null;
+        while (inMatcher.find()) {
+
+            Matcher matcher = getLinkMatcher(inMatcher.group(1));
+            if (matcher.find()) {
+                outStr = matcher.group(1).substring(1,matcher.group(1).length()-1);
+                if (outStr != null && !outStr.isEmpty())
+                    getLinkList().add(outStr);
+            }
+
+        }
+    }
+
+
+}
--- src/main/java/ru/exercise01/util/MakeUrlList.java
+++ src/main/java/ru/exercise01/util/MakeUrlList.java
@@ -1,19 +1,22 @@
 package ru.exercise01.util;
 
-import ru.exercise01.model.UrlList;
 
-import java.io.BufferedReader;
+import org.slf4j.Logger;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Arrays;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * Created by Egor on 18.12.16.
  */
 public class MakeUrlList {
+
+    private  static final Logger LOG = getLogger(MakeUrlList.class);
 
     public static void MakeUrlList(final String inUrl){
         try {
@@ -23,28 +26,12 @@ public class MakeUrlList {
             GetConn.connect();
 
             InputStreamReader ReadIn = new InputStreamReader(GetConn.getInputStream());
-            BufferedReader BufData = new BufferedReader(ReadIn);
-//            String htmlFileName = ("\\hello111.html");
-//            FileWriter FWriter = new FileWriter(htmlFileName);
-//            BufferedWriter BWriter = new BufferedWriter(FWriter);
-            String UrlData = null;
-            while ((UrlData = BufData.readLine()) != null) {
-                Pattern pattern = Pattern.compile("(?i)<a([^>]+)>(.+?)>");
-                Matcher matcher = pattern.matcher(UrlData);
-                if (matcher.find()) {
-                  String eee =  matcher.group(1) ;
-                }
-                for (int i = 1; i < matcher.groupCount()-1; i++) {
-                    UrlList.addUrlList(matcher.group(i));
-
-                }
-//                BWriter.write(UrlData);
-//                BWriter.newLine();
-            }
-//            BWriter.close();
-        }
+             ExtractLinks extractLinks = new ExtractLinks(ReadIn);
+             extractLinks.getLinkList();
+         }
         catch (IOException io) {
-            System.out.println(io);
+            Arrays.stream(io.getStackTrace()).forEach(ee->LOG.info(ee.toString()));
+
         }
     }
 
